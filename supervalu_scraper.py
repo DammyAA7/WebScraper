1a8ababa256c439bb82b439ea7498d64f08848d1
Oluwadamilola Adebayo <o.adebayo2@univerisityofgalway.ie>
2023-12-29 17:39:00 +0000

    tesco and supervalu scraper complete. print to csv file

--- supervalu_scraper.py
+++ supervalu_scraper.py
@@ -7,58 +7,80 @@ def get_value(element, class_name):
     found_element = element.find(class_=class_name)
     return found_element.text.strip() if found_element else 'None'
 
-def scrape_and_update_url(base_url, page_number, skip_value):
+def extract_price(product_title):
+    # Try to find the price in the product title
+    price_start = product_title.find('€')
+    if price_start != -1:
+        # Extract the price substring
+        price_str = product_title[price_start:]
+        return price_str.strip()
+    else:
+        return 'None'
+
+def scrape_and_update_url(base_url, page_number, skip_value, output_file='fruit.csv'):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
-    while True:
-        # Construct the URL for the current page
-        url = f"{base_url}?page={page_number}&skip={skip_value}"
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Product Title', 'Price', 'Price Per Weight', 'Savings', 'Image URL']
+        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Write the header
+        csv_writer.writeheader()
 
-        # Send a GET request to the URL
-        response = requests.get(url, headers=headers)
+        while True:
+            # Construct the URL for the current page
+            url = f"{base_url}?page={page_number}&skip={skip_value}"
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+            # Send a GET request to the URL
+            response = requests.get(url, headers=headers)
 
-            # Find all product cards on the page
-            product_cards = soup.find_all('article', class_='ProductCardWrapper--6uxd5a')
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-            if not product_cards:
-                # If no product cards are found, terminate the loop
-                print("No more products found!")
+                # Find all product cards on the page
+                product_cards = soup.find_all('article', class_='ProductCardWrapper--6uxd5a')
+
+                if not product_cards:
+                    # If no product cards are found, terminate the loop
+                    print("No more products found!")
+                    break
+
+                for card in product_cards:
+                   # Extracting product title (containing price)
+                    product_title = card.find('p', class_='AriaProductTitleParagraph--1yc7f4f').text.strip()
+
+                    # Extracting price from the product title
+                    price = extract_price(product_title)
+
+                    # Removing price from the product title
+                    product_title = product_title.replace(price, '').strip()
+
+                    # Remove double quotes from the product title
+                    product_title = product_title.replace(',', '')
+
+                    # Extracting price per weight
+                    price_per_weight = get_value(card, 'ProductCardPriceInfo--1vvb8df')
+
+                    # Extracting product savings
+                    product_savings = get_value(card, 'Badge--ayjc1w')
+
+                    # Extracting product image URL
+                    product_image_url = card.find('img')['src']
+
+                    # Write the data to CSV
+                    csv_writer.writerow({'Product Title': product_title, 'Price': price, 'Price Per Weight': price_per_weight, 'Savings': product_savings, 'Image URL': product_image_url})
+
+                # Update page_number and skip_value for the next iteration
+                page_number += 1
+                skip_value += 30
+
+                # Introduce a delay of 1.5 seconds before the next request
+                time.sleep(1.5)
+
+            else:
+                print(f"Failed to retrieve the page {page_number}. Status code:", response.status_code)
                 break
-
-            # Print headers
-            if page_number == 1:
-                print("Product Title,Price Per Weight,Savings,Image URL")
-
-            for card in product_cards:
-                # Extracting product title (containing price)
-                product_title = card.find('p', class_='AriaProductTitleParagraph--1yc7f4f').text.strip()
-
-                # Extracting price per weight
-                price_per_weight = get_value(card, 'ProductCardPriceInfo--1vvb8df')
-
-                # Extracting product savings
-                product_savings = get_value(card, 'Badge--ayjc1w')
-
-                # Extracting product image URL
-                product_image_url = card.find('img')['src']
-
-                # Print product information in CSV format
-                print(f'"{product_title}","{price_per_weight}","{product_savings}","{product_image_url}"')
-
-            # Update page_number and skip_value for the next iteration
-            page_number += 1
-            skip_value += 30
-
-            # Introduce a delay of 1.5 seconds before the next request
-            time.sleep(1.5)
-
-        else:
-            print(f"Failed to retrieve the page {page_number}. Status code:", response.status_code)
-            break
 
 # Example usage:
 base_url = "https://shop.supervalu.ie/sm/delivery/rsid/5550/categories/fruit-%26-vegetables/fruit-id-O200001"
-scrape_and_update_url(base_url, page_number=1, skip_value=0)
+scrape_and_update_url(base_url, page_number=1, skip_value=0, output_file='output.csv')
--- tesco_scraper.py
+++ tesco_scraper.py
@@ -1,23 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
+import csv
 
-url = 'https://www.tesco.ie/groceries/en-IE/shop/bakery/in-store-bakery/all'
+def extract_data_from_url(url, output_file='output.csv'):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument("--disable-extensions")
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+    driver = webdriver.Chrome(options=options)
+    page_number = 1
 
-driver = webdriver.Chrome()
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Title', 'Price', 'Price Per', 'Image URL']
+        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        # Write the header
+        csv_writer.writeheader()
 
-driver.get(url)
+        while True:
+            current_url = f"{url}?page={page_number}&count=48"
+            driver.get(current_url)
+
+            time.sleep(1)
+            
+            # Check if the page has products
+            products = driver.find_elements(By.CLASS_NAME, 'styles__StyledVerticalTileWrapper-dvv1wj-0.dtCNPH')
+            if not products:
+                print("No more products found!")
+                break  # No more pages, exit the loop
+
+            for product in products:
+                title = product.find_element(By.CLASS_NAME, "styled__Text-sc-1i711qa-1.xZAYu.ddsweb-link__text").text
+                try:
+                    price = product.find_element(By.CLASS_NAME, "styled__StyledHeading-sc-119w3hf-2.jWPEtj.styled__Text-sc-8qlq5b-1.lnaeiZ.beans-price__text").text
+                except:
+                    price = "Currently out of stock"
+
+                try:
+                    priceper = product.find_element(By.CLASS_NAME, "styled__StyledFootnote-sc-119w3hf-7.icrlVF.styled__Subtext-sc-8qlq5b-2.bNJmdc.beans-price__subtext").text
+                except:
+                    priceper = ""
+
+                image_element = product.find_element(By.CLASS_NAME, "styled__Image-sc-j2gwt2-0.iCtiex.product-image.ddsweb-responsive-image__image")
+                driver.execute_script("arguments[0].scrollIntoView();", image_element)
+                image_url = image_element.get_attribute("src")
+
+                # Write the data to CSV
+                csv_writer.writerow({'Title': title, 'Price': price, 'Price Per': priceper, 'Image URL': image_url})
+
+            time.sleep(2)
+            page_number += 1
+
+    driver.quit()
 
 
-products = driver.find_elements(By.CLASS_NAME, 'styles__StyledVerticalTileWrapper-dvv1wj-0.dtCNPH')
-for product in products:
-    title = product.find_element(By.CLASS_NAME, "styled__Text-sc-1i711qa-1.xZAYu.ddsweb-link__text").text
-    price = product.find_element(By.CLASS_NAME, "styled__StyledHeading-sc-119w3hf-2.jWPEtj.styled__Text-sc-8qlq5b-1.lnaeiZ.beans-price__text").text
-    priceper = product.find_element(By.CLASS_NAME, "styled__StyledFootnote-sc-119w3hf-7.icrlVF.styled__Subtext-sc-8qlq5b-2.bNJmdc.beans-price__subtext").text
-    image_element = product.find_element(By.CLASS_NAME, "styled__Image-sc-j2gwt2-0.iCtiex.product-image.ddsweb-responsive-image__image")
-    # Scroll the image element into view
-    driver.execute_script("arguments[0].scrollIntoView();", image_element)
-    image_url = image_element.get_attribute("src")
-
-
-    print(f'"{title}","{price}","{priceper}","{image_url}"')
+# Specify the output file path if you want to use a different name/location
+output_file_path = 'water.csv'
+url = 'https://www.tesco.ie/groceries/en-IE/shop/drinks/water/all'
+extract_data_from_url(url, output_file_path)
